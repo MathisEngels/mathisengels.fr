a9ef302a30dedd6c47c8eb3f80a3a7f6c66943c4
Mathis Engels <mathisengels@yahoo.fr>
2024-04-15 16:32:47 +0200

    feat: translate project content

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,9 +5,15 @@ const projectCollection = defineCollection({
   schema: ({ image }) => z.object({
     image: image(),
     title: z.string(),
-    description: z.string(),
+    description: z.object({
+        fr: z.string(),
+        en: z.string(),
+    }),
     link: z.string().optional(),
-    type: z.string(),
+    type: z.object({
+        fr: z.enum(['Freelance', 'Personnel', 'Open Source']),
+        en: z.enum(['Freelance', 'Personal', 'Open Source']),
+    }),
     tags: z.array(z.string()),
     date: z.string(),
     displayOnHomepage: z.boolean().optional()
