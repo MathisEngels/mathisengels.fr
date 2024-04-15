3adc089c01f5f65b61cc367edab1ad50faef0361
Mathis Engels <mathisengels@yahoo.fr>
2024-04-15 18:05:03 +0200

    feat: Write the description text

--- src/i18n/translations/aboutMe.ts
+++ src/i18n/translations/aboutMe.ts
@@ -3,9 +3,29 @@ import type { Translation } from '..'
 const translations = {
     en: {
         'aboutMe.title': "About me",
+        'aboutMe.text': 
+        `I'm Mathis, 25 years old, 2 years of experience in development and I have to admit that I'm thriving as a Fullstack developer.
+        <br />
+        <br />
+        Passionate about my job, I use modern technologies and frameworks such as React.js, Next.js, Typescript, and many others, to design innovative solutions.
+        <br />
+        Autonomous, organized and motivated by technical challenges, I invest myself fully in every project.
+        <br />
+        <br />
+        Don't hesitate to contact me!`
     },
     fr: {
         'aboutMe.title': "A propos de moi",
+        'aboutMe.text': 
+        `Moi c'est Mathis, j'ai 25 ans et 2 ans d'expérience dans le domaine du développement, et je dois avouer que je m'épanouis en tant que développeur Fullstack.
+        <br />
+        <br />
+        Passionné par mon métier, j'utilise quotidiennement des technologies et frameworks modernes tel que React.js, Next.js, Typescript, et bien d'autres, pour concevoir des solutions innovantes. 
+        <br />
+        Autonome, organisé et motivé par les défis techniques, je m'investis pleinement dans chaque projet.
+        <br />
+        <br />
+        N'hésitez pas à me contacter !`
     },
 } satisfies Translation
 
