efeda6ead82b6ee946be7269899309863af680b6
Mathis Engels <mathisengels@yahoo.fr>
2024-04-15 17:19:58 +0200

    feat: add sitemap and robots.txt integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import react from '@astrojs/react'
+import sitemap from '@astrojs/sitemap'
 import tailwind from '@astrojs/tailwind'
 import icon from 'astro-icon'
+import robots from 'astro-robots'
 import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon(), react()],
+    integrations: [tailwind(), icon(), react(), sitemap(), robots()],
     i18n: {
         defaultLocale: 'fr',
-        locales: ['fr', 'en']
-    }
+        locales: ['fr', 'en'],
+    },
+    site: 'https://mathisengels.fr',
 })
